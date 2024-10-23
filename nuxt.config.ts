466c4ed7e96f3eeb447ad4ea3e645cb5f2356fd6
ShalltearFR <Jossuha@hotmail.fr>
2024-10-23 21:06:28 +0200

    change cors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
   },
   security: {
     corsHandler: {
-      origin: process.env.WWW as string, // Ajoutez ici les domaines autorisés
+      origin: process.env.WWW,
       methods: ["GET", "POST", "OPTIONS"],
     },
     rateLimiter: {
@@ -47,48 +47,64 @@ export default defineNuxtConfig({
       interval: 60000,
       tokensPerInterval: 10,
     },
-    // headers: {
-    // xXSSProtection: "1",
-    // contentSecurityPolicy: {
-    //   "default-src": ["'self'", "https://clips.twitch.tv"],
-    //   "frame-src": [
-    //     "'self'",
-    //     "https://clips.twitch.tv",
-    //     "https://*.vercel.app",
-    //     "https://vercel.live",
-    //   ],
-    //   "script-src": [
-    //     "'self'",
-    //     "'unsafe-inline'", // Autorise les scripts inline
-    //     "'unsafe-eval'", // Si nécessaire
-    //     "https://clips.twitch.tv",
-    //     "https://*.vercel.app",
-    //     "https://vercel.live",
-    //   ],
-    //   "img-src": [
-    //     "'self'",
-    //     "data:",
-    //     "https://static-cdn.jtvnw.net",
-    //     "*.twitch.tv",
-    //   ],
-    //   "style-src": ["'self'", "'unsafe-inline'"],
-    //   "script-src-elem": [
-    //     "'self'",
-    //     "'unsafe-inline'",
-    //     "'unsafe-eval'",
-    //     "https://clips.twitch.tv",
-    //     "https://*.vercel.app",
-    //     "https://vercel.live", // Ajoutez aussi ici pour les éléments script
-    //   ],
-    //   "script-src-attr": ["'self'", "'unsafe-inline'"],
-    // },
-    // },
-  },
-  routeRules: {
-    "/api/**": {
-      security: {
-        rateLimiter: { headers: true, interval: 60000, tokensPerInterval: 5 },
+    headers: {
+      xXSSProtection: "1",
+      contentSecurityPolicy: {
+        "default-src": ["'self'", "https://clips.twitch.tv"],
+        "frame-src": [
+          "'self'",
+          "https://clips.twitch.tv",
+          "https://*.vercel.app",
+          "https://vercel.live",
+        ],
+        "script-src": [
+          "'self'",
+          "'unsafe-inline'", // Autorise les scripts inline
+          "'unsafe-eval'", // Si nécessaire
+          "https://clips.twitch.tv",
+          "https://*.vercel.app",
+          "https://vercel.live",
+        ],
+        "img-src": [
+          "'self'",
+          "data:",
+          "https://static-cdn.jtvnw.net",
+          "*.twitch.tv",
+        ],
+        "style-src": ["'self'", "'unsafe-inline'"],
+        "script-src-elem": [
+          "'self'",
+          "'unsafe-inline'",
+          "'unsafe-eval'",
+          "https://clips.twitch.tv",
+          "https://*.vercel.app",
+          "https://vercel.live", // Ajoutez aussi ici pour les éléments script
+        ],
+        "script-src-attr": ["'self'", "'unsafe-inline'"],
       },
     },
   },
+  // routeRules: {
+  //   "/api/**": {
+  //     security: {
+  //       rateLimiter: { headers: true, interval: 60000, tokensPerInterval: 5 },
+  //     },
+  //     headers: {
+  //       "Access-Control-Allow-Origin": process.env.WWW as string,
+  //       "Access-Control-Allow-Methods": "GET, POST",
+  //       "Access-Control-Allow-Headers":
+  //         "Origin, X-Requested-With, Content-Type, Accept",
+  //       xXSSProtection: "1",
+  //       "Content-Security-Policy": [
+  //         "default-src 'self' https://clips.twitch.tv",
+  //         "frame-src 'self' https://clips.twitch.tv https://clip-twitch.vercel.app https://vercel.live",
+  //         "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://clips.twitch.tv https://clip-twitch.vercel.app https://vercel.live",
+  //         "img-src 'self' data: https://static-cdn.jtvnw.net *.twitch.tv",
+  //         "style-src 'self' 'unsafe-inline'",
+  //         "script-src-elem 'self' 'unsafe-inline' 'unsafe-eval' https://clips.twitch.tv https://clip-twitch.vercel.app https://vercel.live",
+  //         "script-src-attr 'self' 'unsafe-inline'",
+  //       ].join("; "),
+  //     },
+  //   },
+  // },
 });
