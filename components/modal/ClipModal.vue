11f925cc6fb1c8ffbc51b445dd71af275d11e6b6
ShalltearFR <Jossuha@hotmail.fr>
2024-10-23 18:48:54 +0200

    change parent

--- components/modal/ClipModal.vue
+++ components/modal/ClipModal.vue
@@ -29,7 +29,7 @@
       </div>
       <iframe
         class="rounded-xl"
-        :src="`https://clips.twitch.tv/embed?clip=${data.id}&parent=['clip-twitch.vercel.app']&autoplay=true&width=960&height=540`"
+        :src="`https://clips.twitch.tv/embed?clip=${data.id}&parent=clip-twitch.vercel.app&autoplay=true&width=960&height=540`"
         width="960"
         height="540"
         scrolling="no"
