99e681efc9e80ce148bb064820d963ea5ddef334
ShalltearFR <Jossuha@hotmail.fr>
2024-10-23 20:21:38 +0200

    disabling cors for testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
     "@nuxtjs/i18n",
     "@nuxt/image",
     "@nuxtjs/google-fonts",
-    "nuxt-security",
+    // "nuxt-security",
   ],
   i18n: {
     locales: [
@@ -37,66 +37,66 @@ export default defineNuxtConfig({
       cookieKey: "lang",
     },
   },
-  security: {
-    corsHandler: {
-      origin: process.env.WWW as string,
-      methods: ["GET"],
-    },
-    rateLimiter: {
-      headers: true,
-      interval: 60000,
-      tokensPerInterval: 10,
-    },
-    headers: {
-      xXSSProtection: "1",
-      contentSecurityPolicy: {
-        "default-src": ["'self'", "https://clips.twitch.tv"],
-        "frame-src": [
-          "'self'",
-          "https://clips.twitch.tv",
-          "https://*.vercel.app",
-          "https://vercel.live",
-        ],
-        "script-src": [
-          "'self'",
-          "'unsafe-inline'", // Autorise les scripts inline
-          "'unsafe-eval'", // Si nécessaire
-          "https://clips.twitch.tv",
-          "https://*.vercel.app",
-          "https://vercel.live",
-        ],
-        "img-src": [
-          "'self'",
-          "data:",
-          "https://static-cdn.jtvnw.net",
-          "*.twitch.tv",
-        ],
-        "style-src": ["'self'", "'unsafe-inline'"],
-        "script-src-elem": [
-          "'self'",
-          "'unsafe-inline'",
-          "'unsafe-eval'",
-          "https://clips.twitch.tv",
-          "https://*.vercel.app",
-          "https://vercel.live", // Ajoutez aussi ici pour les éléments script
-        ],
-        "script-src-attr": ["'self'", "'unsafe-inline'"],
-      },
-    },
-  },
-  routeRules: {
-    "/api/**": {
-      security: {
-        rateLimiter: { headers: true, interval: 60000, tokensPerInterval: 5 },
-      },
+  // security: {
+  //   corsHandler: {
+  //     origin: process.env.WWW as string,
+  //     methods: ["GET"],
+  //   },
+  //   rateLimiter: {
+  //     headers: true,
+  //     interval: 60000,
+  //     tokensPerInterval: 10,
+  //   },
+  //   headers: {
+  //     xXSSProtection: "1",
+  //     contentSecurityPolicy: {
+  //       "default-src": ["'self'", "https://clips.twitch.tv"],
+  //       "frame-src": [
+  //         "'self'",
+  //         "https://clips.twitch.tv",
+  //         "https://*.vercel.app",
+  //         "https://vercel.live",
+  //       ],
+  //       "script-src": [
+  //         "'self'",
+  //         "'unsafe-inline'", // Autorise les scripts inline
+  //         "'unsafe-eval'", // Si nécessaire
+  //         "https://clips.twitch.tv",
+  //         "https://*.vercel.app",
+  //         "https://vercel.live"
+  //       ],
+  //       "img-src": [
+  //         "'self'",
+  //         "data:",
+  //         "https://static-cdn.jtvnw.net",
+  //         "*.twitch.tv",
+  //       ],
+  //       "style-src": ["'self'", "'unsafe-inline'"],
+  //       "script-src-elem": [
+  //         "'self'",
+  //         "'unsafe-inline'",
+  //         "'unsafe-eval'",
+  //         "https://clips.twitch.tv",
+  //         "https://*.vercel.app",
+  //         "https://vercel.live", // Ajoutez aussi ici pour les éléments script
+  //       ],
+  //       "script-src-attr": ["'self'", "'unsafe-inline'"],
+  //     },
+  //   },
+  // },
+  // routeRules: {
+  //   "/api/**": {
+  //     security: {
+  //       rateLimiter: { headers: true, interval: 60000, tokensPerInterval: 5 },
+  //     },
 
-      headers: {
-        "Access-Control-Allow-Origin": process.env.WWW as string,
-        "Access-Control-Allow-Methods": "GET, POST",
-        "Access-Control-Allow-Headers":
-          "Origin, X-Requested-With, Content-Type, Accept",
-        xXSSProtection: "1",
-      },
-    },
-  },
+  //     headers: {
+  //       "Access-Control-Allow-Origin": process.env.WWW as string,
+  //       "Access-Control-Allow-Methods": "GET, POST",
+  //       "Access-Control-Allow-Headers":
+  //         "Origin, X-Requested-With, Content-Type, Accept",
+  //       xXSSProtection: "1",
+  //     },
+  //   },
+  // },
 });
